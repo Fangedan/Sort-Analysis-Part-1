24122e1fc83eccf61c1400f6f199ce874b50f02f
Andrew Lin <andrew.m.lin@student.allenisd.org>
2022-10-06 09:51:48 -0500

    sort

--- InsertionSort/main.swift
+++ InsertionSort/main.swift
@@ -1,39 +1,40 @@
-import Foundation
+var unsortedStrings = [String]();
 
-var array = unsortedIntegers
-
-var pass = 0
-var swaptotal = 0
-var store = 0
-var swap = 0
-
-func insertionSort<T:Comparable>(array: inout [T])
-{
-    for numplace in 0..<array.count
-    {
-        let value = array[numplace]
-        var numbefore = numplace - 1
-        while numbefore >= 0
-        {
-            swap = 0
-            if array[numbefore] > value
-            {
-                array[numbefore+1] = array[numbefore]
-                swaptotal += 1
-                swap += 1
-            }
-            else
-            {
-                break
-            }
-            numbefore -= 1
-            store += swap
-        }
-        array[numbefore+1] = value
-        pass += 1
-        print("Pass: \(pass-1), Swaps: \(store)/\(swaptotal), Array:",array)
-        store = 0
-    }
+while let line = readLine() {
+    if(line.count == 0) {break;}
+    unsortedStrings.append(line)
 }
 
-insertionSort(array: &array)
+// Add your code below:
+
+func swap(integers: inout [String], firstIndex:Int, secondIndex:Int) { // Swap two places in an integer array
+    let temp = integers[secondIndex]
+    integers[secondIndex] = integers[firstIndex]
+    integers[firstIndex] = temp
+}
+
+var strings = unsortedStrings
+
+var passes = 0;
+var totalSwaps = 0;
+
+print("Pass: \(passes), Swaps: 0/\(totalSwaps), Array: \(strings)")
+
+// loop through the array starting at 1 (so has something to compare to 1-1 = 0)
+for i in 1..<strings.count {
+    var swaps = 0
+    var j = i;
+
+    // loops through the current index until the start of the array (works backwards), comparing two elements each time
+    while j > 0 && strings[j] < strings[j - 1] {
+        swap(integers: &strings, firstIndex: j - 1, secondIndex: j)
+        j -= 1
+        swaps += 1
+        totalSwaps += 1;
+    }
+
+    // prints the wanted output
+    passes += 1;
+    //    print("Pass: \(passes), Swaps: \(swaps)/\(totalSwaps), Array: \(strings)")
+}
+print(strings)
