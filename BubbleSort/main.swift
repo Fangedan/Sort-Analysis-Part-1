f2e786fc6736b1a43e3df2468e80c02a334d8743
Andrew Lin <andrew.m.lin@student.allenisd.org>
2022-10-07 17:48:53 -0500

    update

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -1,19 +1,21 @@
-var unsortedStrings = [String]();
+var newString = [String]();
 
 while let line = readLine() {
-    if(line.count == 0) {break;}
-    unsortedStrings.append(line)
+    if(line.count == 0)
+    {
+        break;
+    }
+    newString.append(line)
 }
 
-// Add your code below:
-
-func swap(integers: inout [String], firstIndex:Int, secondIndex:Int) { // Swap two places in an integer array
-    let temp = integers[secondIndex]
+func swap(integers: inout [String], firstIndex:Int, secondIndex:Int)
+{
+    let store = integers[secondIndex]
     integers[secondIndex] = integers[firstIndex]
-    integers[firstIndex] = temp
+    integers[firstIndex] = store
 }
 
-var integers = unsortedStrings
+var integers = newString
 
 var passes = 0;
 var totalSwaps = 0;
@@ -21,18 +23,20 @@ var sort = true;
 
 print("Pass: \(passes), Swaps: 0/\(totalSwaps), Array: \(integers)")
 
-while sort == true { // while found something to swap (if didn't find anything, stop looping)
+while sort == true
+{
     var swaps = 0
     sort = false
-    for i in 0..<integers.count-1 { // go for each element in the array except the last one.
-        if(integers[i] > integers[i+1]) { // if the current element is greater than the next, swap them.
+    for i in 0..<integers.count-1
+    {
+        if(integers[i] > integers[i+1])
+        {
             sort = true
-            swaps += 1 // add one to the swaps for the step
-            totalSwaps += 1 // add one to the total amount of swaps for the bubble sort
-            swap(integers:&integers, firstIndex:i, secondIndex:i+1) // actually swap the two elements
+            swaps += 1
+            totalSwaps += 1
+            swap(integers:&integers, firstIndex:i, secondIndex:i+1)
         }
     }
-    passes += 1 // increment how many steps/passes it has taken.
-    //    print("Pass: \(passes), Swaps: \(swaps)/\(totalSwaps), Array: \(integers)") // print out the required format
+    passes += 1
 }
 print(integers)
